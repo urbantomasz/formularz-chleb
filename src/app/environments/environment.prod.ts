464d0c195207f731431058dd56b5456b736ba5e5
Tomasz Urban <urbantomasz94@gmail.com>
2025-04-06 13:08:13 +0200

    redirect change

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'https://mojepszczoly-h6crb8dragdhfhe7.polandcentral-01.azurewebsites.net/api',
-    redirectUri: 'https://urbantomasz.github.io/moje-pszczoly-app',
+    redirectUri: 'https://urbantomasz.github.io/formularz-chleb',
   };
