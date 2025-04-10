5e255623a96ab6907e616ad17c6e0c25fae54b73
Tomasz Urban <urbantomasz94@gmail.com>
2025-04-11 00:32:58 +0200

    layout changes

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'https://mojepszczoly-h6crb8dragdhfhe7.polandcentral-01.azurewebsites.net/api',
-    redirectUri: 'https://urbantomasz.github.io/formularz-chleb',
+    redirectUri: window.location.origin,
   };
